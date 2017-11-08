30937decf702563ebdf3634c661b2670e2a121c9
sdiaz186 <33470917+sdiaz186@users.noreply.github.com>
2017-11-07 20:17:07 -0500

    Add files via upload
    add files via upload

---
+++ Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamGames;
+
+namespace TeamGames
+{
+    public class Game
+    {
+        public string teamOne { get; set; }
+
+        public string teamTwo { get; set; }
+
+        public int teamOneScore { get; set; }
+
+        public int teamTwoScore { get; set; }
+
+        public Game() { }
+
+        public Game(string teamOne, string teamTwo, int teamOneScore, int teamTwoScore)
+        {
+            this.teamOne = teamOne;
+            this.teamTwo = teamTwo;
+            this.teamOneScore = teamOneScore;
+            this.teamTwoScore = teamTwoScore;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1}) - {2} ({3})", teamOne, teamOneScore, teamTwo, teamTwoScore);
+        }
+    }
+}
